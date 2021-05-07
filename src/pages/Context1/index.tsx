e01684de550e1dda6abe38d68416c0c6ff92e97d
jinliang <zhangjinliang@gmail.com>
2021-05-07 10:06:00 -0400

    '

--- src/pages/Context1/index.tsx
+++ src/pages/Context1/index.tsx
@@ -7,25 +7,30 @@ export interface Context1Props {}
 
 const Context1: React.FC<Context1Props> = () => {
   const srcs = [
-    "https://github.com/zhiqinghu/portfolio/blob/master/public/assets/project1/1.jpg",
-    "https://github.com/zhiqinghu/portfolio/blob/master/public/assets/project1/2.jpg",
-    "https://github.com/zhiqinghu/portfolio/blob/master/public/assets/project1/3.jpg",
-    "https://github.com/zhiqinghu/portfolio/blob/master/public/assets/project1/4.jpg",
-    "https://github.com/zhiqinghu/portfolio/blob/master/public/assets/project1/5.jpg",
-    "https://github.com/zhiqinghu/portfolio/blob/master/public/assets/project1/6.JPG",
-    "https://github.com/zhiqinghu/portfolio/blob/master/public/assets/project1/7.JPG",
-    "https://github.com/zhiqinghu/portfolio/blob/master/public/assets/project1/8.JPG",
-    "https://github.com/zhiqinghu/portfolio/blob/master/public/assets/project1/cover.jpg",
-    "https://github.com/zhiqinghu/portfolio/blob/master/public/assets/project1/10.JPG",
-    "https://github.com/zhiqinghu/portfolio/blob/master/public/assets/project1/11.jpg",
-    "https://github.com/zhiqinghu/portfolio/blob/master/public/assets/project1/12.jpg",
-    "https://github.com/zhiqinghu/portfolio/blob/master/public/assets/project1/13.jpg",
-    "https://github.com/zhiqinghu/portfolio/blob/master/public/assets/project1/14.jpg",
+    "./assets/project1/1.jpg",
+    "./assets/project1/2.jpg",
+    "./assets/project1/3.jpg",
+    "./assets/project1/4.jpg",
+    "./assets/project1/5.jpg",
+    "./assets/project1/6.JPG",
+    "./assets/project1/7.JPG",
+    "./assets/project1/8.JPG",
+    "./assets/project1/cover.jpg",
+    "./assets/project1/10.JPG",
+    "./assets/project1/11.jpg",
+    "./assets/project1/12.jpg",
+    "./assets/project1/13.jpg",
+    "./assets/project1/14.jpg",
   ];
   const title = "Continuous Space";
   const content =
     "This project had been seeking for the relation between vertical plan, horizontal plan and spaces through the exploration of three parts. Continuous space is a theme in the pursuing of the connection and conceptual ideas of the space. Overall, it is seeking the beauty of archicture, space and lights and powerfulness of the traditional way in demonstar tion of spaces.";
-  return <Context srcs={srcs} content={content} title={title} />;
+  return (
+    <>
+      <Context srcs={srcs} content={content} title={title} />
+      <img src="/assets/project1/1.jpg" />
+    </>
+  );
 };
 
 export default Context1;
--- src/pages/Context3/index.tsx
+++ src/pages/Context3/index.tsx
@@ -6,16 +6,16 @@ export interface Context3Props {}
 
 const Context3: React.FC<Context3Props> = () => {
   const srcs = [
-    "https://github.com/zhiqinghu/portfolio/blob/master/public/assets/project3/1.jpg",
-    "https://github.com/zhiqinghu/portfolio/blob/master/public/assets/project3/2.jpg",
-    "https://github.com/zhiqinghu/portfolio/blob/master/public/assets/project3/3.jpg",
-    "https://github.com/zhiqinghu/portfolio/blob/master/public/assets/project3/cover.jpg",
-    "https://github.com/zhiqinghu/portfolio/blob/master/public/assets/project3/5.jpg",
-    "https://github.com/zhiqinghu/portfolio/blob/master/public/assets/project3/6.jpg",
-    "https://github.com/zhiqinghu/portfolio/blob/master/public/assets/project3/7.jpg",
-    "https://github.com/zhiqinghu/portfolio/blob/master/public/assets/project3/8.jpg",
-    "https://github.com/zhiqinghu/portfolio/blob/master/public/assets/project3/9.jpg",
-    "https://github.com/zhiqinghu/portfolio/blob/master/public/assets/project3/10.jpg",
+    "./assets/project3/1.jpg",
+    "./assets/project3/2.jpg",
+    "./assets/project3/3.jpg",
+    "./assets/project3/cover.jpg",
+    "./assets/project3/5.jpg",
+    "./assets/project3/6.jpg",
+    "./assets/project3/7.jpg",
+    "./assets/project3/8.jpg",
+    "./assets/project3/9.jpg",
+    "./assets/project3/10.jpg",
   ];
   const title = "Why In Person";
   const content =
